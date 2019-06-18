525afa54bda72092a62b9d112e176cb1453447d3
hardeepkaur <hardeepwalia019@gmail.com>
2019-06-18 18:45:32 -0400

    initial commit

--- OrangeTree/GameScene.swift
+++ OrangeTree/GameScene.swift
@@ -10,6 +10,7 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+     var oranges:[SKSpriteNode] = []
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
@@ -17,73 +18,65 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         
         // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
+        }
+    func makeOrange(xPosition:CGFloat, yPosition:CGFloat) {
+        // lets add some cats
+        let orange = SKSpriteNode(imageNamed: "Orange")
+        
+        // set the position of the orange
+        orange.position = CGPoint(x:xPosition, y:yPosition)
+        orange.zPosition = 100
+        
+        // add physics to the orange
+        // -------------------------
+        
+        // 1. create a circular hitbox around the orange
+        orange.physicsBody = SKPhysicsBody(circleOfRadius: orange.size.width / 2)
+        // By default, a physics body has:
+        // -- dynamic = true (show collisions)
+        // -- affectedByGravity = true;
+        
+        
+        // add the orange to the scene
+        addChild(orange)
+        
+        // add the oragne to the oranges array
+        self.oranges.append(orange)
+        
+        print("Where is orange? \(xPosition), \(yPosition)")
+    }
         // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        
+        // detect when you touch the tree
+        let locationTouched = touches.first
+        
+        if (locationTouched == nil) {
+            // This is error handling
+            // Sometimes the mouse detection doesn't work properly
+            // and IOS can't get the position.
+            return
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
+        let mousePosition = locationTouched!.location(in:self)
+        
+        print("MOUSE X?  \(mousePosition.x)")
+        print("MOUSE Y?  \(mousePosition.y)")
+        print("------")
+        
+        
+
+    // 1. Detect what sprite the person touched
+    let spriteTouched = self.atPoint(mousePosition)
+    //print("YOU TOUCHED: \(spriteTouched)")
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    // 2. Check if he touched the tree
+    if (spriteTouched.name == "tree") {
+    print("TREE TOUCHED!")
+         makeOrange(xPosition: mousePosition.x, yPosition: mousePosition.y)
     }
+        
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+}
 }
